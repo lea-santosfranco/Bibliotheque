1cfde3049d92ef2baa38f7e9d38bf1701f490b47
Manondev11 <manon.elm@hotmail.com>
2026-03-05 16:02:04 +0100

    projet en cours

--- src/Controller/EmpruntController.php
+++ src/Controller/EmpruntController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Entity\Emprunt;
+use App\Form\EmpruntType;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,21 +17,45 @@ final class EmpruntController extends AbstractController
     #[Route('/emprunt', name: 'app_emprunt')]
     public function index(): Response
     {
+        // $emprunts = $empruntRepository->findAll();
+
         return $this->render('emprunt/index.html.twig', [
-            'controller_name' => 'EmpruntController',
+            'controller_name' => 'Liste des Emprunts',
         ]);
     }
+
     #[Route('/emprunt/new/{id}', name: 'app_emprunt_new')]
     public function new(
         Book $book,
         Request $request,
         EntityManagerInterface $entityManagerInterface
     ): Response {
-        if ($book->getStock()<0){
+        if ($book->getStock() < 0) {
             $this->addFlash('danger', 'Livre non disponible');
-            
+
             return $this->redirectToRoute('app_book_index');
         }
-        $emprunt =
+        $emprunt = new Emprunt();
+        $emprunt->setBook($book);
+        $emprunt->setDateEmprunt(new \DateTimeImmutable());
+        $emprunt->setStatut('en_cours');
+
+        $form = $this->createForm(EmpruntType::class, $emprunt);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book->setStock($book->getStock() - 1);
+
+            $entityManagerInterface->persist($emprunt);
+            $entityManagerInterface->flush();
+
+            $this->addFlash('success', "Emprunt enregistré !");
+
+            return $this->redirectToRoute('app_book_index');
+        }
+        return $this->render('emprunt/new.html.twig', [
+            'book' => $book,
+            'form' => $form->createView(),
+        ]);
     }
 }

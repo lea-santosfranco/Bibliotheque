6111e0f296d2ece3d3f3258be1f8adaf490abb4e
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-03-09 09:34:52 +0100

    test

--- migrations/Version20260309083127.php
+++ migrations/Version20260309083127.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260306093333 extends AbstractMigration
+final class Version20260309083127 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,6 @@ final class Version20260306093333 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE book ADD image VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE emprunt ADD CONSTRAINT FK_364071D716A2B381 FOREIGN KEY (book_id) REFERENCES book (id)');
         $this->addSql('ALTER TABLE emprunt ADD CONSTRAINT FK_364071D7A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
     }
@@ -28,7 +27,6 @@ final class Version20260306093333 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE book DROP image');
         $this->addSql('ALTER TABLE emprunt DROP FOREIGN KEY FK_364071D716A2B381');
         $this->addSql('ALTER TABLE emprunt DROP FOREIGN KEY FK_364071D7A76ED395');
     }
